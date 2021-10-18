8c3df2a85cb559c9ca54d26f352f111065393a8e
mitchelbade <mitchelm.bade@gmail.com>
2021-10-17 23:50:03 -0700

    commit tree 40e66d950f3acff1adfb4fcd4c672fe809d2eeb9

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+    
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(star = '*') {
+    return function(name='special') {
+        return `You are ${star}${name}${star}!`;
+    }
+}
+
+saturdayFun();
+mondayWork();
+wrapAdjective();
